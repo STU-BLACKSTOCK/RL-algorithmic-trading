72e7eba3b109f6ace2db1a44a94563fdc685c992
Vishal V <vishal20jan2005@gmail.com>
2026-06-13 16:30:36 +0530

    Added PPO training pipeline

---
+++ src/train.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_checker import check_env
+
+from trading_env import TradingEnv
+
+df = pd.read_csv(
+    "../data/train/AAPL.csv"
+)
+
+env = TradingEnv(df)
+
+check_env(env)
+
+print("Environment validation passed.")
+
+model = PPO(
+    policy="MlpPolicy",
+    env=env,
+    verbose=1,
+    tensorboard_log="../logs/tensorboard/"
+)
+
+model.learn(
+    total_timesteps=10000
+)
+
+model.save(
+    "../models/ppo_aapl_v1"
+)
+
+print("Training completed.")
