3c806918be6ff93502b34b45a660cd24eeb6e830
Vishal V <vishal20jan2005@gmail.com>
2026-06-15 21:42:47 +0530

    Add PPO prediction endpoint

--- backend/routes/model_routes.py
+++ backend/routes/model_routes.py
@@ -4,6 +4,10 @@ from backend.services.model_service import (
     ModelService
 )
 
+from backend.schemas.prediction_schema import (
+    PredictionRequest
+)
+
 router = APIRouter()
 
 service = ModelService()
@@ -13,3 +17,21 @@ service = ModelService()
 def get_model_info():
 
     return service.get_model_info()
+
+@router.post("/predict")
+def predict(
+    request: PredictionRequest
+):
+
+    action = (
+        service.predict_action()
+    )
+
+    return {
+        "ticker":
+            request.ticker,
+        "action":
+            service.action_to_text(
+                action
+            )
+    }
--- backend/services/model_service.py
+++ backend/services/model_service.py
@@ -1,14 +1,45 @@
+from stable_baselines3 import PPO
 import os
+import numpy as np
+
+# Project root directory
+BASE_DIR = os.path.dirname(
+    os.path.dirname(
+        os.path.dirname(
+            os.path.abspath(__file__)
+        )
+    )
+)
+
+# Model path
+MODEL_PATH = os.path.join(
+    BASE_DIR,
+    "models",
+    "ppo_aapl_v7"
+)
 
 
 class ModelService:
 
     def __init__(self):
 
+        self.model_path = MODEL_PATH
+
+        # Load trained PPO model
+        self.model = PPO.load(
+            self.model_path
+        )
+
         self.model_name = "ppo_aapl_v7"
 
         self.lookback_window = 30
 
+        # Actions:
+        # 0 = Hold
+        # 1 = Buy 25%
+        # 2 = Buy 100%
+        # 3 = Sell 25%
+        # 4 = Sell 100%
         self.action_space = 5
 
     def get_model_info(self):
@@ -16,5 +47,42 @@ class ModelService:
         return {
             "model": self.model_name,
             "lookback_window": self.lookback_window,
-            "action_space": self.action_space
+            "action_space": self.action_space,
+            "loaded": True
         }
+
+    def predict_action(self):
+
+        obs_size = (
+            self.lookback_window * 12
+        ) + 2
+
+        observation = np.zeros(
+            obs_size,
+            dtype=np.float32
+        )
+
+        action, _ = self.model.predict(
+            observation,
+            deterministic=True
+        )
+
+        return int(action)
+
+    def action_to_text(
+    self,
+    action
+    ):
+
+        mapping = {
+            0: "HOLD",
+            1: "BUY_25",
+            2: "BUY_100",
+            3: "SELL_25",
+            4: "SELL_100"
+        }
+
+        return mapping.get(
+            action,
+            "UNKNOWN"
+        )
